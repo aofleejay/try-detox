ad738ed22a12b88e9e57cbaf03b8da978d5bff02
aofleejay <aofleejay@hotmail.com>
2018-08-01 18:37:17 +0700

    Downgrade to react-native@0.50.4

--- App.js
+++ App.js
@@ -19,11 +19,10 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component<{}> {
   render() {
     return (
-      <View testID="welcome" style={styles.container}>
+      <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
