809bf3cfde88c4d77d581c52483d451168b7738d
aofleejay <aofleejay@hotmail.com>
2018-08-06 15:46:55 +0700

    Add redux-persist

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Provider } from 'react-redux'
-import store from './redux/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import createStore from './redux/store'
 import Navigation from './Navigation'
+
+const { store, persistor } = createStore()
 
 const App = () => (
   <Provider store={store}>
-    <Navigation />
+    <PersistGate loading={null} persistor={persistor}>
+      <Navigation />
+    </PersistGate>
   </Provider>
 )
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
+import { AsyncStorage } from 'react-native'
 import { createStore, combineReducers } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
 import foo from './foo'
+
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+}
 
 const reducers = combineReducers({
   foo,
 })
 
-const store = createStore(reducers)
+const persistedReducer = persistReducer(persistConfig, reducers)
 
-export default store
+export default () => {
+  const store = createStore(persistedReducer)
+  const persistor = persistStore(store)
+  return { store, persistor }
+}
